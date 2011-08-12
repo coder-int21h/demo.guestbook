60122a76a68c33e24a79e1f220d9d9da93ca7f81
Sergey Tkachenko <coder.int21h@gmail.com>
2011-08-12 15:27:13 +0300

    configurated main.php

---
+++ www/protected/config/main.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'demo.guestbook',
+    'defaultController' => 'post',
+    'import' => array(
+        'application.models.*',
+        'application.components.*',
+    ),
+    /**
+     * 
+     */
+    'components' => array(
+        'db' => array(
+            'connectionString' => 'sqlite:protected/data/guestbook.db',
+        ),
+    ),
+);
+?>
