23a4d06b3061b9f1b6bc0c748182d3f2d8275138
Sergey Tkachenko <coder.int21h@gmail.com>
2011-08-12 15:22:02 +0300

    added UserIdentity class

---
+++ www/protected/components/UserIdentity.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UserIdentity
+ *
+ * @author Admin
+ */
+class UserIdentity extends CUserIdentity
+{
+
+    private $_id;
+
+    public function authenticate()
+    {
+        $record = User::model()->findByAttributes(array('username' => $this->username));
+        if ($record === null)
+            $this->errorCode = self::ERROR_USERNAME_INVALID;
+        else if ($record->password !== md5($this->password))
+            $this->errorCode = self::ERROR_PASSWORD_INVALID;
+        else
+        {
+            $this->_id = $record->id;
+            $this->setState('title', $record->title);
+            $this->errorCode=self::ERROR_NONE;
+        }
+        return !$this->errorCode;
+    }
+
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+}
+
+?>
